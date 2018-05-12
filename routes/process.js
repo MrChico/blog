65b36e70891108832a931cb137f2921b61e08271
Erik <eeckee@gmail.com>
2018-05-12 14:21:01 +0200

    added

--- routes/process.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-	res.render('process', { 
-			title0: 'Process'
-	})
-});
-module.exports = router;
